c6b8f871328abf8750bc50218ab9376e86e14774
Sérgio Borges <sbintcel123@gmail.com>
2024-09-10 22:29:27 -0300

    Operadores e listas

---
+++ fundamentos/lib/variaveis/listas/1_declaracao.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Declaração e inicialização de uma lista de inteiros
+  // Tipos Genéricos
+  List<int> listaNumeros = [1, 2, 3];
+
+  List<String> listaTextos = ['Fulano', 'Beltrano', 'Sicrano'];
+
+  // Declaração por inferência e atribuição
+  var listaNumerosInferencia = [1, 2, 3];
+
+  var listaTextosInferencia = ['Fulano', 'Beltrano', 'Sicrano'];
+
+  // Declaração de lista vazia
+
+  List<int> listaSemDadosGenerico = [];
+
+  var listaSemDadosInferencia = [];
+
+  var listaSemDadosInferenciaGenerico = <int>[];
+
+  var listaSemDadosInferenciaGenericoString = <String>[];
+}
